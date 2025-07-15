100b5d72a1df8058c9374de5fc40baba424d3a17
Nikolay Nikolov <nickysn@gmail.com>
2025-07-15 18:37:05 +0300

    feat(stream_api): update test test_binary_roundtrip to use the new API

--- runtime_tracing/tests/binary_format.rs
+++ runtime_tracing/tests/binary_format.rs
@@ -1,4 +1,4 @@
-use runtime_tracing::{TraceEventsFileFormat, Tracer};
+use runtime_tracing::{create_trace_reader, create_trace_writer, TraceEventsFileFormat};
 use std::path::Path;
 use std::fs;
 
@@ -6,27 +6,27 @@ use std::fs;
 fn test_binary_roundtrip() {
     let json_path = Path::new("tests/data/trace.json");
 
-    let mut tracer = Tracer::new("", &[]);
-    tracer
-        .load_trace_events(json_path, TraceEventsFileFormat::Json)
+    let mut json_reader = create_trace_reader(TraceEventsFileFormat::Json);
+    let original = json_reader
+        .load_trace_events(json_path)
         .unwrap();
-    let original = tracer.events.clone();
 
     let bin_path = Path::new("tests/data/trace.bin");
 
-    tracer
-        .store_trace_events(bin_path, TraceEventsFileFormat::Binary)
-        .unwrap();
+    let mut bin_writer = create_trace_writer("", &[], TraceEventsFileFormat::Binary);
+    bin_writer.begin_writing_trace_events(bin_path).unwrap();
+    bin_writer.append_events(&mut original.clone());
+    bin_writer.finish_writing_trace_events().unwrap();
 
-    let mut tracer2 = Tracer::new("", &[]);
-    tracer2
-        .load_trace_events(bin_path, TraceEventsFileFormat::Binary)
+    let mut bin_reader = create_trace_reader(TraceEventsFileFormat::Binary);
+    let tracer2_events = bin_reader
+        .load_trace_events(bin_path)
         .unwrap();
 
     fs::remove_file(bin_path).unwrap();
 
     let orig_json = serde_json::to_string(&original).unwrap();
-    let new_json = serde_json::to_string(&tracer2.events).unwrap();
+    let new_json = serde_json::to_string(&tracer2_events).unwrap();
 
     assert_eq!(orig_json, new_json);
 }

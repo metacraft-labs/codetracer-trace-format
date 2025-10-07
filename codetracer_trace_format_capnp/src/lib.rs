a5de208f31d06c7dcd4ac9112c426f562b39b688
Nikolay Nikolov <nickysn@gmail.com>
2025-10-07 15:03:05 +0300

    feat(separate_trace_types_crate): Added non_streaming_trace_writer to codetracer_trace_writer; supports writing JSON and capnp formats

--- codetracer_trace_format_capnp/src/lib.rs
+++ codetracer_trace_format_capnp/src/lib.rs
@@ -1,4 +1,4 @@
-mod capnptrace;
+pub mod capnptrace;
 
 pub mod trace_capnp {
     include!(concat!(env!("OUT_DIR"), "/src/trace_capnp.rs"));
--- codetracer_trace_writer/src/lib.rs
+++ codetracer_trace_writer/src/lib.rs
@@ -1,2 +1,10 @@
 mod abstract_trace_writer;
+mod non_streaming_trace_writer;
 mod trace_writer;
+
+#[derive(Debug, Clone, Copy)]
+pub enum TraceEventsFileFormat {
+    Json,
+    BinaryV0,
+    Binary,
+}

d3c4523a8a314c732046fcedc9760494c6dd44c9
Nikolay Nikolov <nickysn@gmail.com>
2025-10-07 15:12:47 +0300

    feat(separate_trace_types_crate): Added cbor_zstd_writer to codetracer_trace_writer - supports writing CBOR+ZSTD files

--- codetracer_trace_writer/src/lib.rs
+++ codetracer_trace_writer/src/lib.rs
@@ -2,6 +2,13 @@ mod abstract_trace_writer;
 mod non_streaming_trace_writer;
 mod trace_writer;
 
+#[cfg(target_arch = "wasm32")]
+#[path = "./cbor_zstd_writer_wasm.rs"]
+mod cbor_zstd_writer;
+
+#[cfg(not(target_arch = "wasm32"))]
+mod cbor_zstd_writer;
+
 #[derive(Debug, Clone, Copy)]
 pub enum TraceEventsFileFormat {
     Json,
